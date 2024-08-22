a496074d318fa86b31a825dab142bb2994e9f489
furkan aydin <furknn2112@gmail.com>
2024-08-22 22:22:15 +0300

    feat(app.component): Add Button component to app component
    - Create Button component

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,39 @@ import logo from './logo.svg';
 import './App.css';
 import { getProducts, Product } from "./data";
 
+enum Mission {
+  Previous = 'Previos',
+  Next = 'Next'
+}
+
+type ButtonProps = {
+  mission: Mission
+  onButtonClick?: (e: React.MouseEvent<HTMLButtonElement>, mission: Mission) => void
+}
+
+function Button({ mission, onButtonClick }: ButtonProps) {
+
+  return <button type='button' className='next-button' onClick={(e) => onButtonClick && onButtonClick(e, mission)} >{mission}</button>
+}
+
 function App() {
   const [productIndex, setProductIndex] = useState(0);
 
   const products: Product[] = getProducts();
 
-  function handleButtonClick() {
-    if ((productIndex + 1) >= products.length) return;
-
-    setProductIndex(productIndex + 1);
-    console.log(`productIndex variable after click the button: ${productIndex}`);
+  function handleButtonClick(e: React.MouseEvent<HTMLButtonElement>, mission: Mission): void {
+    if (mission === Mission.Next) {
+      if ((productIndex + 1) >= products.length) return;
+      setProductIndex(productIndex + 1);
+    } else if (mission === Mission.Previous) {
+      if ((productIndex - 1) < 0) return;
+      setProductIndex(productIndex - 1);
+    }
   }
 
   return (
     <div className='product-segment'>
-      <button type='button' className='next-button' onClick={handleButtonClick}>next</button>
+      <Button mission={Mission.Previous} onButtonClick={handleButtonClick} />
       <div className='product-card'>
         <div className='card-header'>Product Card</div>
         <div>
@@ -26,6 +44,7 @@ function App() {
           <p><b>Product Quantity:</b> {products[productIndex].quantity}</p>
         </div>
       </div>
+      <Button mission={Mission.Next} onButtonClick={handleButtonClick} />
     </div>
   );
 }
